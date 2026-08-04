805816a84f17df94cad11af4d87f8ae2248d7193
ddandu <devendra2402@gmail.com>
2026-08-05 01:31:40 +0530

    terraform-dev-infra code

--- 40-databases/locals.tf
+++ 40-databases/locals.tf
@@ -5,7 +5,7 @@ locals {
 #   redis_sg_id = data.aws_ssm_parameter.redis_sg_id.value
 #   rabbitmq_sg_id = data.aws_ssm_parameter.rabbitmq_sg_id.value
 #   mysql_sg_id = data.aws_ssm_parameter.mysql_sg_id.value
-  ami_id = data.aws_ami.devresh.id
+  ami_id = data.aws_ami.devreshops.id
   common_tags = {
       Project = var.project_name
       Environment = var.environment
