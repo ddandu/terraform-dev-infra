1c6e2aa39a82549f58cbc427850b831ca9a590b9
ddandu <devendra2402@gmail.com>
2026-08-05 12:00:48 +0530

    terraform db changes

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -3,4 +3,4 @@
 dnf install ansible -y
 #ansible-pull -U https://github.com/daws-86s/ansible-roboshop-roles-tf.git -e component=$component main.yaml
 
-ansible-pull -U https://github.com/ddandu/ansible-roboshop-roles-terraform.git -e component=$component main.yaml
+ansible-pull -U https://github.com/ddandu/ansible-roboshop-roles-terraform.git -e component=mongodb main.yaml
