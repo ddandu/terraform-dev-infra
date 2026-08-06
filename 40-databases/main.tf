5c96d0588738b926f0d4f1e680e6205c229e65dd
ddandu <devendra2402@gmail.com>
2026-08-07 01:13:14 +0530

    terrafrom dev project

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -164,7 +164,7 @@ resource "terraform_data" "mysql" {
   provisioner "remote-exec" {
     inline = [
         "chmod +x /tmp/bootstrap.sh",
-        "sudo sh /tmp/bootstrap.sh mysql dev"
+        "sudo sh /tmp/bootstrap.sh mysql"
     ]
   }
 }
