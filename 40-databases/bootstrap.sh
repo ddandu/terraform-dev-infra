07481f16369e2af5ff8cff720ffd91995f4b4e92
ddandu <devendra2402@gmail.com>
2026-08-07 01:23:59 +0530

    terrafrom dev project

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 component=$1
-#environment=$2
+environment=$2
 dnf install ansible -y
-
 #ansible-pull -U https://github.com/daws-86s/ansible-roboshop-roles-tf.git -e component=$component main.yaml
 # git clone ansible-playbook
 # cd ansible-playbook
@@ -30,5 +29,4 @@ else
     cd $ANSIBLE_DIR
 fi
 
-
-ansible-playbook -e component=$component -e environment=dev main.yaml
+ansible-playbook -e component=$component environment=$environment main.yaml
