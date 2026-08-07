84800e1261a711ee15381312e4d0e619e9b89dc5
ddandu <devendra2402@gmail.com>
2026-08-07 18:59:34 +0530

    databases change for

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -1,6 +1,7 @@
 locals {
   common_name_suffix = "${var.project_name}-${var.environment}" # roboshop-dev
   private_subnet_id = split("," , data.aws_ssm_parameter.private_subnet_ids.value)[0]
+  catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
   ami_id = data.aws_ami.devreshops.id
   common_tags = {
       Project = var.project_name
