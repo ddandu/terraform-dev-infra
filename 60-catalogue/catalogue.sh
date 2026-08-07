238020c1d88c05917b546673f8f3de5991e61401
ddandu <devendra2402@gmail.com>
2026-08-07 21:07:07 +0530

    backend changes

--- 60-catalogue/catalogue.sh
+++ 60-catalogue/catalogue.sh
@@ -4,9 +4,9 @@ component=$1
 environment=$2
 dnf install ansible -y
 
-REPO_URL=https://github.com/daws-86s/ansible-roboshop-roles-tf.git
+PO_URL=https://github.com/ddandu/ansible-roboshop-roles-terraform.git 
 REPO_DIR=/opt/roboshop/ansible
-ANSIBLE_DIR=ansible-roboshop-roles-tf
+ANSIBLE_DIR=ansible-roboshop-roles-terraform
 
 mkdir -p $REPO_DIR
 mkdir -p /var/log/roboshop/
