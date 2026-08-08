2b3d95ea284c645c304a306d62133c0c04f8e6db
ddandu <devendra2402@gmail.com>
2026-08-08 12:22:14 +0530

    catalogue chanes

--- 50-backend-alb/variables.tf
+++ 50-backend-alb/variables.tf
@@ -5,3 +5,11 @@ variable "project_name" {
 variable "environment" {
     default = "dev"
 }
+
+variable "zone_id" {
+    default = "Z08998102AQ5DAQ38NK0N"
+}
+
+variable "domain_name" {
+    default = "devresh.online"
+}
