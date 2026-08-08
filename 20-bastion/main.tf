aaebb39a77bd34bf6990542c9ebcd66498222f36
ddandu <devendra2402@gmail.com>
2026-08-08 11:09:55 +0530

    catalogue chanes

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -3,7 +3,15 @@ resource "aws_instance" "bastion" {
     instance_type = "t3.micro"
     vpc_security_group_ids = [local.bastion_sg_id] 
     subnet_id = local.public_subnet_id
-    iam_instance_profile = aws_iam_instance_profile.bastion.name
+    iam_instance_profile = aws_iam_instance_profile.bastion.name #Attache the role to EC2 instace
+
+    # need more for terraform
+    root_block_device {
+        volume_size = 50
+        volume_type = "gp3" # or "gp2", depending on your preference
+    }
+
+
      user_data = file("bastion.sh") #aws will create instace and file is function it read bastio.sh fil and send the command to aws
 
     tags = merge (
@@ -15,6 +23,7 @@ resource "aws_instance" "bastion" {
 }
 
 
+#Attache the role to EC2 instace
 resource "aws_iam_instance_profile" "bastion" {
   name = "bastion"
   role = "BastionTerrformAdmin"
--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -1,8 +1,10 @@
 locals {
   common_name_suffix = "${var.project_name}-${var.environment}" # roboshop-dev
   private_subnet_id = split("," , data.aws_ssm_parameter.private_subnet_ids.value)[0]
+  private_subnet_ids = split("," , data.aws_ssm_parameter.private_subnet_ids.value)
   catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
   ami_id = data.aws_ami.devreshops.id
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
   common_tags = {
       Project = var.project_name
       Environment = var.environment
