d02738a66ba92b3ca45d5b93aa540518b936f19b
ddandu <devendra2402@gmail.com>
2026-08-14 13:16:40 +0530

    added dev code

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -29,25 +29,25 @@ resource "aws_security_group_rule" "mongodb_bastion" {
 }
 
 
-# # Refis accepting connections from bastion on port 22
-# resource "aws_security_group_rule" "redis_bastion" {
-#   type              = "ingress"
-#   security_group_id = local.redis_sg_id
-#   source_security_group_id = local.bastion_sg_id
-#   from_port         = 22
-#   protocol          = "tcp"
-#   to_port           = 22
-# }
+# Refis accepting connections from bastion on port 22
+resource "aws_security_group_rule" "redis_bastion" {
+  type              = "ingress"
+  security_group_id = local.redis_sg_id
+  source_security_group_id = local.bastion_sg_id
+  from_port         = 22
+  protocol          = "tcp"
+  to_port           = 22
+}
 
 
-# resource "aws_security_group_rule" "rabbitmq_bastion" {
-#   type              = "ingress"
-#   security_group_id = local.rabbitmq_sg_id
-#   source_security_group_id = local.bastion_sg_id
-#   from_port         = 22
-#   protocol          = "tcp"
-#   to_port           = 22
-# }
+resource "aws_security_group_rule" "rabbitmq_bastion" {
+  type              = "ingress"
+  security_group_id = local.rabbitmq_sg_id
+  source_security_group_id = local.bastion_sg_id
+  from_port         = 22
+  protocol          = "tcp"
+  to_port           = 22
+}
 
 
 # # Created as part of ticket 1234GDF
