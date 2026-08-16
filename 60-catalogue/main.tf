573c03414b14b56c27d68e0aa5301068e5539f7e
ddandu <devendra2402@gmail.com>
2026-08-16 19:27:16 +0530

    add database changes

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -13,175 +13,175 @@ resource "aws_instance" "catalogue" {
     )
 }
 
-# Connect to instance using remote-exec provisioner through terraform_data
-resource "terraform_data" "catalogue" {
-  triggers_replace = [
-    aws_instance.catalogue.id
-  ]
+# # Connect to instance using remote-exec provisioner through terraform_data
+# resource "terraform_data" "catalogue" {
+#   triggers_replace = [
+#     aws_instance.catalogue.id
+#   ]
   
-  connection {
-    type     = "ssh"
-    user     = "ec2-user"
-    password = "DevOps321"
-    host     = aws_instance.catalogue.private_ip
-  }
+#   connection {
+#     type     = "ssh"
+#     user     = "ec2-user"
+#     password = "DevOps321"
+#     host     = aws_instance.catalogue.private_ip
+#   }
 
-  # terraform copies this file to catalogue server
-  provisioner "file" {
-    source = "catalogue.sh"
-    destination = "/tmp/catalogue.sh"
-  }
+#   # terraform copies this file to catalogue server
+#   provisioner "file" {
+#     source = "catalogue.sh"
+#     destination = "/tmp/catalogue.sh"
+#   }
 
-  provisioner "remote-exec" {
-    inline = [
-        "chmod +x /tmp/catalogue.sh",
-        # "sudo sh /tmp/catalogue.sh"
-        "sudo sh /tmp/catalogue.sh catalogue ${var.environment}"
-    ]
-  }
-}
+#   provisioner "remote-exec" {
+#     inline = [
+#         "chmod +x /tmp/catalogue.sh",
+#         # "sudo sh /tmp/catalogue.sh"
+#         "sudo sh /tmp/catalogue.sh catalogue ${var.environment}"
+#     ]
+#   }
+# }
 
-#3 stop the instance
-# stop the instance to take image
-resource "aws_ec2_instance_state" "catalogue" {
-  instance_id = aws_instance.catalogue.id
-  state       = "stopped"
-  depends_on = [terraform_data.catalogue]
-}
+# #3 stop the instance
+# # stop the instance to take image
+# resource "aws_ec2_instance_state" "catalogue" {
+#   instance_id = aws_instance.catalogue.id
+#   state       = "stopped"
+#   depends_on = [terraform_data.catalogue]
+# }
 
-#4 take AMI -> devops-practice is the base AMI
-resource "aws_ami_from_instance" "catalogue" {
-  name  = "${local.common_name_suffix}-catalogue-ami"
-  source_instance_id = aws_instance.catalogue.id
-  depends_on = [aws_ec2_instance_state.catalogue]
-  tags = merge (
-        local.common_tags,
-        {
-            Name = "${local.common_name_suffix}-catalogue-ami" # roboshop-dev-mongodb
-        }
-  )
-}
+# #4 take AMI -> devops-practice is the base AMI
+# resource "aws_ami_from_instance" "catalogue" {
+#   name  = "${local.common_name_suffix}-catalogue-ami"
+#   source_instance_id = aws_instance.catalogue.id
+#   depends_on = [aws_ec2_instance_state.catalogue]
+#   tags = merge (
+#         local.common_tags,
+#         {
+#             Name = "${local.common_name_suffix}-catalogue-ami" # roboshop-dev-mongodb
+#         }
+#   )
+# }
 
-resource "aws_lb_target_group" "catalogue" {
-  name     = "${local.common_name_suffix}-catalogue"
-  port     = 8080
-  protocol = "HTTP"
-  vpc_id   = local.vpc_id
-  deregistration_delay = 60 # waiting period before deleting the instance
+# resource "aws_lb_target_group" "catalogue" {
+#   name     = "${local.common_name_suffix}-catalogue"
+#   port     = 8080
+#   protocol = "HTTP"
+#   vpc_id   = local.vpc_id
+#   deregistration_delay = 60 # waiting period before deleting the instance
 
-  health_check {
-    healthy_threshold = 2
-    interval = 10
-    matcher = "200-299"
-    path = "/health"
-    port = 8080
-    protocol = "HTTP"
-    timeout = 2
-    unhealthy_threshold = 2
-  }
-}
+#   health_check {
+#     healthy_threshold = 2
+#     interval = 10
+#     matcher = "200-299"
+#     path = "/health"
+#     port = 8080
+#     protocol = "HTTP"
+#     timeout = 2
+#     unhealthy_threshold = 2
+#   }
+# }
 
 
-resource "aws_launch_template" "catalogue" {
-  name = "${local.common_name_suffix}-catalogue"
-  image_id = aws_ami_from_instance.catalogue.id
+# resource "aws_launch_template" "catalogue" {
+#   name = "${local.common_name_suffix}-catalogue"
+#   image_id = aws_ami_from_instance.catalogue.id
 
-  instance_initiated_shutdown_behavior = "terminate"
-  instance_type = "t3.micro"
+#   instance_initiated_shutdown_behavior = "terminate"
+#   instance_type = "t3.micro"
 
-  vpc_security_group_ids = [local.catalogue_sg_id]
+#   vpc_security_group_ids = [local.catalogue_sg_id]
 
-  # when we run terraform apply again, a new version will be created with new AMI ID
-  update_default_version = true
+#   # when we run terraform apply again, a new version will be created with new AMI ID
+#   update_default_version = true
 
-  # tags attached to the instance
-  tag_specifications {
-    resource_type = "instance"
+#   # tags attached to the instance
+#   tag_specifications {
+#     resource_type = "instance"
 
-    tags = merge(
-      local.common_tags,
-      {
-        Name = "${local.common_name_suffix}-catalogue"
-      }
-    )
-  }
+#     tags = merge(
+#       local.common_tags,
+#       {
+#         Name = "${local.common_name_suffix}-catalogue"
+#       }
+#     )
+#   }
 
-   # tags attached to the volume created by instance
-  tag_specifications {
-    resource_type = "volume"
+#    # tags attached to the volume created by instance
+#   tag_specifications {
+#     resource_type = "volume"
 
-    tags = merge(
-      local.common_tags,
-      {
-        Name = "${local.common_name_suffix}-catalogue"
-      }
-    )
-  }
+#     tags = merge(
+#       local.common_tags,
+#       {
+#         Name = "${local.common_name_suffix}-catalogue"
+#       }
+#     )
+#   }
 
-  # tags attached to the launch template
-  tags = merge(
-      local.common_tags,
-      {
-        Name = "${local.common_name_suffix}-catalogue"
-      }
-  )
+#   # tags attached to the launch template
+#   tags = merge(
+#       local.common_tags,
+#       {
+#         Name = "${local.common_name_suffix}-catalogue"
+#       }
+#   )
 
-}
+# }
 
-resource "aws_autoscaling_group" "catalogue" {
-  name                      = "${local.common_name_suffix}-catalogue"
-  max_size                  = 10
-  min_size                  = 1
-  health_check_grace_period = 100
-  health_check_type         = "ELB"
-  desired_capacity          = 1
-  force_delete              = false
-  launch_template {
-    id      = aws_launch_template.catalogue.id
-    version = aws_launch_template.catalogue.latest_version
-  }
-  vpc_zone_identifier       = local.private_subnet_ids
-  target_group_arns = [aws_lb_target_group.catalogue.arn]
+# resource "aws_autoscaling_group" "catalogue" {
+#   name                      = "${local.common_name_suffix}-catalogue"
+#   max_size                  = 10
+#   min_size                  = 1
+#   health_check_grace_period = 100
+#   health_check_type         = "ELB"
+#   desired_capacity          = 1
+#   force_delete              = false
+#   launch_template {
+#     id      = aws_launch_template.catalogue.id
+#     version = aws_launch_template.catalogue.latest_version
+#   }
+#   vpc_zone_identifier       = local.private_subnet_ids
+#   target_group_arns = [aws_lb_target_group.catalogue.arn]
 
-  instance_refresh {
-    strategy = "Rolling"
-    preferences {
-      min_healthy_percentage = 50 # atleast 50% of the instances should be up and running
-    }
-    triggers = ["launch_template"]
-  }
+#   instance_refresh {
+#     strategy = "Rolling"
+#     preferences {
+#       min_healthy_percentage = 50 # atleast 50% of the instances should be up and running
+#     }
+#     triggers = ["launch_template"]
+#   }
   
-  dynamic "tag" {  # we will get the iterator with name as tag
-    for_each = merge(
-      local.common_tags,
-      {
-        Name = "${local.common_name_suffix}-catalogue"
-      }
-    )
-    content {
-      key                 = tag.key
-      value               = tag.value
-      propagate_at_launch = true
-    }
-  }
+#   dynamic "tag" {  # we will get the iterator with name as tag
+#     for_each = merge(
+#       local.common_tags,
+#       {
+#         Name = "${local.common_name_suffix}-catalogue"
+#       }
+#     )
+#     content {
+#       key                 = tag.key
+#       value               = tag.value
+#       propagate_at_launch = true
+#     }
+#   }
 
-  timeouts {
-    delete = "15m"
-  }
+#   timeouts {
+#     delete = "15m"
+#   }
 
-}
+# }
 
 
-resource "aws_autoscaling_policy" "catalogue" {
-  autoscaling_group_name = aws_autoscaling_group.catalogue.name
-  name                   = "${local.common_name_suffix}-catalogue"
-  policy_type            = "TargetTrackingScaling"
+# resource "aws_autoscaling_policy" "catalogue" {
+#   autoscaling_group_name = aws_autoscaling_group.catalogue.name
+#   name                   = "${local.common_name_suffix}-catalogue"
+#   policy_type            = "TargetTrackingScaling"
 
-  target_tracking_configuration {
-    predefined_metric_specification {
-      predefined_metric_type = "ASGAverageCPUUtilization"
-    }
+#   target_tracking_configuration {
+#     predefined_metric_specification {
+#       predefined_metric_type = "ASGAverageCPUUtilization"
+#     }
 
-    target_value = 75.0
-  }
-}
+#     target_value = 75.0
+#   }
+# }
