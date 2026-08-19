4aac41d7c2a381609461259f2ed23fb0023487cf
ddandu <devendra2402@gmail.com>
2026-08-19 22:43:31 +0530

    added code

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -80,4 +80,13 @@ resource "aws_security_group_rule" "mongodb_catalogue" {
   to_port           = 27017
 }
 
- 
+
+#catalgoe accept connection from backend alb
+resource "aws_security_group_rule" "catalogue_backend_alb" {
+  type              = "ingress"
+  security_group_id = local.catalogue_sg_id
+  source_security_group_id = local.backend_alb_sg_id
+  from_port         = 8080
+  protocol          = "tcp"
+  to_port           = 8080
+}
