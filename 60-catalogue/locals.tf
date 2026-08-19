f067b4e389b3e07414ae1eb7806c51daed892d59
ddandu <devendra2402@gmail.com>
2026-08-19 19:11:48 +0530

    added code

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -3,6 +3,7 @@ locals {
   private_subnet_id = split("," , data.aws_ssm_parameter.private_subnet_ids.value)[0]
   private_subnet_ids = split("," , data.aws_ssm_parameter.private_subnet_ids.value)
   catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
+  backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
   ami_id = data.aws_ami.devreshops.id
     vpc_id = data.aws_ssm_parameter.vpc_id.value
   common_tags = {
